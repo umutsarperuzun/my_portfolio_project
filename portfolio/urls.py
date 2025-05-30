42d9610cae82ef1873014743dd6c02063087127c
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-05-31 00:53:50 +0100

    Complete navbar HTML structure with working page links and view routing

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns=[
     path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('blog/',views.blog,name='blog'),
+    path('contact/',views.contact,name='contact'),
+    path('projects/',views.projects,name='projects'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,"portfolio/home.html")
+
+def about(request):
+    return render(request,"portfolio/about.html")
+
+def blog(request):
+    return render(request,"portfolio/blog.html")
+
+def projects(request):
+    return render(request,"portfolio/projects.html")
+
+def contact(request):
+    return render(request,"portfolio/contact.html")
