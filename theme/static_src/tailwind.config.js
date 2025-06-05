7496214e1053b2ad56394a3349a571277263ea52
Umut Sarper Uzun <sarperuzun1@gmail.com>
2025-06-05 19:33:57 +0100

    Add hero section intro animation with left-to-right black overlay transition

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -6,69 +6,77 @@
  */
 
 module.exports = {
-    content: [
-        /**
-         * HTML. Paths to Django template files that will contain Tailwind CSS classes.
-         */
+  content: [
+    /**
+     * HTML. Paths to Django template files that will contain Tailwind CSS classes.
+     */
 
-        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
-        '../templates/**/*.html',
+    /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
+    "../templates/**/*.html",
 
-        /*
-         * Main templates directory of the project (BASE_DIR/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../templates/**/*.html',
+    /*
+     * Main templates directory of the project (BASE_DIR/templates).
+     * Adjust the following line to match your project structure.
+     */
+    "../../templates/**/*.html",
 
-        /*
-         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../**/templates/**/*.html',
+    /*
+     * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
+     * Adjust the following line to match your project structure.
+     */
+    "../../**/templates/**/*.html",
 
-        /**
-         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
-         * patterns match your project structure.
-         */
-        /* JS 1: Ignore any JavaScript in node_modules folder. */
-        // '!../../**/node_modules',
-        /* JS 2: Process all JavaScript files in the project. */
-        // '../../**/*.js',
+    /**
+     * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
+     * patterns match your project structure.
+     */
+    /* JS 1: Ignore any JavaScript in node_modules folder. */
+    // '!../../**/node_modules',
+    /* JS 2: Process all JavaScript files in the project. */
+    // '../../**/*.js',
 
-        /**
-         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
-         * and make sure the pattern below matches your project structure.
-         */
-        // '../../**/*.py'
-    ],
-    theme: {
+    /**
+     * Python: If you use Tailwind CSS classes in Python, uncomment the following line
+     * and make sure the pattern below matches your project structure.
+     */
+    // '../../**/*.py'
+  ],
+  theme: {
     extend: {
-        keyframes: {
-            slideInRight: {
-                '0%': { opacity: '0', transform: 'translateX(500px)' },
-                '100%': { opacity: '1', transform: 'translateX(0)' },
-            },
-            slideInLeft: {
-                '0%': { opacity: '0', transform: 'translateX(-500px)' },
-                '100%': { opacity: '1', transform: 'translateX(0)' },
-            },
+        backgroundSize: {
+            '200':'200% 200%',
         },
-        animation: {
-            'slide-in': 'slideInRight 0.8s ease-out',
-            'slide-left': 'slideInLeft 0.8s ease-out',
+      keyframes: {
+        slideInRight: {
+          "0%": { opacity: "0", transform: "translateX(500px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
         },
+        slideInLeft: {
+          "0%": { opacity: "0", transform: "translateX(-500px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+        slideBackground: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '100%': { backgroundPosition: '100% 50%' },
+        },
+      },
+      animation: {
+        "slide-in": "slideInRight 0.8s ease-out",
+        "slide-left": "slideInLeft 0.8s ease-out",
+        "slideBg": 'slideBackground 4s ease-out forwards',
+      },
     },
-},
+  },
 
-    plugins: [
-        /**
-         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-         * for forms. If you don't like it or have own styling for forms,
-         * comment the line below to disable '@tailwindcss/forms'.
-         */
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/line-clamp'),
-        require('@tailwindcss/aspect-ratio'),
-    ],
-}
+  plugins: [
+    /**
+     * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
+     * for forms. If you don't like it or have own styling for forms,
+     * comment the line below to disable '@tailwindcss/forms'.
+     */
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
